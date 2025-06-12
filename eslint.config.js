08ae9501c9c39200abe8afa69503e73b72cea69a
jhelom <1127424+jhelom@users.noreply.github.com>
2025-06-13 00:20:16 +0900

    Update ESLint configuration to include BASE_PATH as a global variable

--- eslint.config.js
+++ eslint.config.js
@@ -8,76 +8,77 @@ import reactPlugin from 'eslint-plugin-react'
 import parser from '@typescript-eslint/parser';
 
 export default tseslint.config(
-  {
-    ignores: [
-      'dist',
-      'src/api/generated'
-    ]
-  },
-  {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: {
-        ...globals.browser,
-        ...globals.jest,
-        GIT_BRANCH: 'readonly',
-        GIT_COMMIT: 'readonly',
-        BUILD_DATE: 'readonly'
-      },
-      parser: parser,
+    {
+        ignores: [
+            'dist',
+            'src/api/generated'
+        ]
     },
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-      'react': reactPlugin,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'import': importPlugin,
-      'eslint-comments': (await import('eslint-plugin-eslint-comments')).default,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...tseslint.configs.recommended[0].rules,
-      ...reactHooks.configs.recommended.rules,
-      ...importPlugin.configs.recommended.rules,
-      ...importPlugin.configs.typescript.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      'no-console': 'error',
-      'semi': ['error', 'always'],
-      'curly': ['error', 'all'],
-      'quotes': ['error', 'single', { avoidEscape: true }],
-      'eqeqeq': ['error', 'always'],
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      'import/order': [
-        'warn',
-        {
-          'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-          'newlines-between': 'always'
-        }
-      ],
-      'react/jsx-uses-react': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react/jsx-boolean-value': ['error', 'never'],
-      'react/self-closing-comp': 'error',
-      'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never' }],
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      'eslint-comments/no-unlimited-disable': 'error',
-      // 'import/no-unresolved': 'warn',
-    },
-    settings: {
-      'import/resolver': {
-        typescript: {
-          project: './tsconfig.json',
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: {
+                ...globals.browser,
+                ...globals.jest,
+                GIT_BRANCH: 'readonly',
+                GIT_COMMIT: 'readonly',
+                BUILD_DATE: 'readonly',
+                BASE_PATH: 'readonly'
+            },
+            parser: parser,
         },
-        node: {
-          extensions: ['.js', '.jsx', '.ts', '.tsx']
-        }
-      },
-    },
-  }
+        plugins: {
+            '@typescript-eslint': tseslint.plugin,
+            'react': reactPlugin,
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            'import': importPlugin,
+            'eslint-comments': (await import('eslint-plugin-eslint-comments')).default,
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...tseslint.configs.recommended[0].rules,
+            ...reactHooks.configs.recommended.rules,
+            ...importPlugin.configs.recommended.rules,
+            ...importPlugin.configs.typescript.rules,
+            'react-refresh/only-export-components': [
+                'warn',
+                {allowConstantExport: true},
+            ],
+            'no-console': 'error',
+            'semi': ['error', 'always'],
+            'curly': ['error', 'all'],
+            'quotes': ['error', 'single', {avoidEscape: true}],
+            'eqeqeq': ['error', 'always'],
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': ['warn', {argsIgnorePattern: '^_'}],
+            'import/order': [
+                'warn',
+                {
+                    'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+                    'newlines-between': 'always'
+                }
+            ],
+            'react/jsx-uses-react': 'off',
+            'react/react-in-jsx-scope': 'off',
+            'react/jsx-boolean-value': ['error', 'never'],
+            'react/self-closing-comp': 'error',
+            'react/jsx-curly-brace-presence': ['error', {props: 'never', children: 'never'}],
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+            'eslint-comments/no-unlimited-disable': 'error',
+            // 'import/no-unresolved': 'warn',
+        },
+        settings: {
+            'import/resolver': {
+                typescript: {
+                    project: './tsconfig.json',
+                },
+                node: {
+                    extensions: ['.js', '.jsx', '.ts', '.tsx']
+                }
+            },
+        },
+    }
 );
